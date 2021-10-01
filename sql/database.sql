f2eda33007867471ce7e4e4cb06996b9d531d435
许德森 <2237803016@qq.com>
2021-10-01 22:27:41 +0800

    update: 更新创建数据库的sql代码

--- sql/database.sql
+++ sql/database.sql
@@ -1,12 +1,14 @@
 create database qqrobot;
 
-create table if not exists t_user
+use qqrobot;
+
+create table t_user
 (
-    qq                    varchar(20)          not null,
-    last_sign             date                 not null,
-    sign_ignore           tinyint(1) default 0 null,
-    group_code            varchar(20)          not null,
-    role                  int        default 1 null comment '1:普通用户, 2:管理员, 3:超级管理员'
+    qq          varchar(20)          not null,
+    last_sign   date                 not null,
+    group_code  varchar(20)          not null,
+    role        int        default 1 null comment '1:普通用户, 2:管理员, 3:超级管理员',
+    sign_ignore tinyint(1) default 0 null
 );
 
 create index GROUP_INDEX
@@ -15,7 +17,8 @@ create index GROUP_INDEX
 create index QQ_INDEX
     on t_user (qq);
 
-create table if not exists t_statistic_group
+
+create table t_statistic_group
 (
     id         int auto_increment
         primary key,
@@ -28,11 +31,11 @@ create index GROUP_CODE_INDEX
     on t_statistic_group (group_code);
 
 
-create table if not exists t_statistic
+create table t_statistic
 (
     id                 int auto_increment
         primary key,
-    content            varchar(100) not null,
+    content            varchar(500) not null,
     qq                 varchar(20)  not null,
     statistic_group_id int          not null
 );
@@ -40,4 +43,15 @@ create table if not exists t_statistic
 create index T_STATISTIC_GROUP_ID
     on t_statistic (statistic_group_id);
 
+create table t_auto_sign
+(
+    qq     varchar(20)          not null
+        primary key,
+    token  varchar(250)         not null,
+    active tinyint(1) default 0 null,
+    yhm    varchar(12)          not null comment '学号',
+    lxdh   varchar(13)          not null comment '学生联系电话',
+    jjlxr  varchar(13)          not null comment '家长名称',
+    jjlxdh varchar(13)          not null comment '家长联系电话'
+);
 
